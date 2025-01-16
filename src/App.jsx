77b8bbd67a3ea9e1c0969580d49615c997a2b388
Ramesh   Devkota <drhsemar@gmail.com>
2025-01-16 11:27:30 -0500

    few updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,52 @@
 // src/App.jsx
-import React from "react";
-import { ThemeProvider, useTheme } from "./contexts/ThemeContext";
+import React, { lazy, Suspense } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import About from "./components/About";
 import Skills from "./components/Skills";
-import SkillsChart from "./components/SkillsChart";
-import Projects from "./components/Projects";
-
-import Timeline from "./components/Timeline";
-import LiveCodeEditor from "./components/LiveCodeEditor";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Background3D from "./components/Background3D";
-// import BlogPost from "./components/BlogPost";
-import "./styles.css";
 import SocialProfiles from "./components/SocialProfiles";
+import Footer from "./components/Footer";
+import "./styles.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { FaAlignCenter } from "react-icons/fa";
-function AppContent() {
-  const { isDarkMode } = useTheme();
 
-  // Sample blog post data
-  const blogPosts = [
-    {
-      id: 1,
-      title: "IS AI Curse Or Blessing??",
-      date: "2024-10-21",
-      author: "Ramesh Devkota",
-      content: "Blogs are underway. Coming soon!!! ",
-    },
-  ];
+// Lazy load components
+const Projects = lazy(() => import("./components/Projects"));
+const Timeline = lazy(() => import("./components/Timeline"));
+const Contact = lazy(() => import("./components/Contact"));
 
+function App() {
   return (
-    <div className={`App ${isDarkMode ? "dark-mode" : "light-mode"}`}>
-      <Background3D />
-
+    <>
       <Header />
       <main>
-        <SocialProfiles />
         <Hero />
 
-        <SkillsChart />
-        <About />
         <section id="skills">
           <h2>My Skills</h2>
           <Skills />
         </section>
-        {/* <section id="blogs">
-          <h2>Blogs</h2>
-          {blogPosts.map((post) => (
-            <BlogPost
-              title={post.title}
-              key={post.id}
-              date={post.date}
-              author={post.author}
-              content={post.content}
-            />
-          ))}
-        </section> */}
 
-        <section id="projects">
-          <h2>My Projects</h2>
-          <Projects />
-        </section>
-        <section id="experience">
-          <h2>My Experience</h2>
-          <Timeline />
-        </section>
-        <section
-          id="code-sample"
-          style={{
-            alignItems: "center",
-            margin: "40px",
-            fontSize: "20px",
-          }}
-        >
-          <h2>Live Code Editor</h2>
-          <LiveCodeEditor />
-        </section>
-        <Contact />
+        <Suspense fallback={<div>Loading projects...</div>}>
+          <section id="projects">
+            <h2>My Projects</h2>
+            <Projects />
+          </section>
+        </Suspense>
+
+        <Suspense fallback={<div>Loading experience...</div>}>
+          <section id="experience">
+            <h2>My Experience</h2>
+            <Timeline />
+          </section>
+        </Suspense>
+
+        <Suspense fallback={<div>Loading contact form...</div>}>
+          <Contact />
+        </Suspense>
       </main>
+
       <SocialProfiles />
       <Footer />
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <ThemeProvider>
-      <AppContent />
-    </ThemeProvider>
+    </>
   );
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import backgroundImage from "../assets/b1.jpg"; // Import your image
 
 const Hero = () => {
+  const heroStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  };
+
   return (
-    <section id="home" className="hero">
+    <section id="home" className="hero" style={heroStyle}>
       <h1>Ramesh Devkota</h1>
       <h2>Full Stack Developer and Digital Creator</h2>
       <a href="tel:+4373760407" className="cta-button">
